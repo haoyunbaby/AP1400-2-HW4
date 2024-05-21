02dff5dff7beb0934502d9fd10afe0bd302ecb84
Xing Zhikai <haoyunbaobei1209@gmail.com>
2024-05-21 18:25:00 +0800

    Update ans
    pass all unit test

--- include/unique_ptr.h
+++ include/unique_ptr.h
@@ -1,14 +1,24 @@
 #ifndef UNIQUE_PTR
 #define UNIQUE_PTR
+template <typename T>
+class UniquePtr {
+ private:
+  T *_p;
 
-
-
-
-
-
-
-
-
-
-
-#endif //UNIQUE_PTR
+ public:
+  UniquePtr();
+  UniquePtr(T *ptr);
+  ~UniquePtr();
+  UniquePtr(const UniquePtr &) = delete;
+  UniquePtr &operator=(const UniquePtr &) = delete;
+  T *get() const;
+  T &operator*() const;
+  T *operator->() const;
+  void reset(T *ptr = nullptr);
+  T *release();
+  operator bool() const;
+};
+template <typename T>
+UniquePtr<T> make_unique(const T &elem);
+#include "unique_ptr.hpp"
+#endif  // UNIQUE_PTR
