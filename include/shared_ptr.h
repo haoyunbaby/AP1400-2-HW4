531d69867978ee969a81487f64991d3517821e93
Xing Zhikai <haoyunbaobei1209@gmail.com>
2024-05-21 18:22:16 +0800

    Update ans
    pass all unit test

--- include/shared_ptr.h
+++ include/shared_ptr.h
@@ -1,5 +1,25 @@
 #ifndef SHARED_PTR
 #define SHARED_PTR
+template <typename T>
+class SharedPtr {
+ private:
+  T *_p;
+  unsigned *ref_count;
 
-
-#endif //SHARED_PTR
+ public:
+  SharedPtr();
+  SharedPtr(T *ptr);
+  ~SharedPtr();
+  SharedPtr(const SharedPtr &other);
+  SharedPtr &operator=(const SharedPtr &other);
+  unsigned use_count() const;
+  T *get() const;
+  T &operator*() const;
+  T *operator->() const;
+  void reset(T *ptr = nullptr);
+  operator bool() const;
+};
+template <typename T>
+SharedPtr<T> make_shared(const T &elem);
+#include "shared_ptr.hpp"
+#endif  // SHARED_PTR
